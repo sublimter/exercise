b10278e982ed35e5c70e88c69ed48c8e3e4012ad
luoliangliang <luoliangliang@huami.com>
2018-05-28 18:03:17 +0800

    初始化工程.

---
+++ Podfile
@@ -0,0 +1,13 @@
+
+platform :ios, '9.0'
+
+target 'swiftDemo' do
+  use_frameworks!
+
+  pod 'Alamofire'
+  pod 'IGListKit'
+  pod 'SwiftyJSON'
+  pod 'HandyJSON'
+  pod 'Kingfisher'
+  pod 'SVProgressHUD'
+end
---
+++ swiftDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//  ViewController.swift
+//  Created on 2018/5/28
+//  Description <#文件描述#>
+
+//  Copyright © 2018年 Huami inc. All rights reserved.
+//  @author luoliangliang(luoliangliang@huami.com)
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
